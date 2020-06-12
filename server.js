a2b3b84cf83a9ffce0cb2ec8bb8d7a0d770e67a1
diefach <diego_sewo@hotmail.com>
2020-06-11 23:42:14 -0300

    [v.07] Added new calculation to process angle

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ var ctrl = new autonomy.Controller(mission.client(), { debug: false });
 
 mission.client().config('general:navdata_demo', 'FALSE');
 
-var targetLat, targetLon, targetYaw, currentLat, currentLon, currentDistance, currentYaw, phoneAccuracy, distance, droneBearing, droneAbsoluteBearing, displacementVector, yawAdjustment, displacement;
+var targetLat, targetLon, targetYaw, currentLat, currentLon, currentDistance, currentYaw, phoneAccuracy, yawAdjustment, distance, droneBearing, droneAbsoluteBearing, displacementVector, displacement;
 var battery = 0;
 
 var stop = function () {
@@ -90,7 +90,7 @@ var stop = function () {
   targetYaw = null
   targetLat = null
   targetLon = null
-  mission.client().stop();
+  mission.client().stop()
 };
 
 // Get the drone absolute bearing
@@ -100,28 +100,45 @@ let getDroneAbsoluteBearing = function () {
 
 let run = function () {
 
-  //Instant commands to waiting and load navdata necessary variables
-  ctrl.up(2);
-  ctrl.hover(10000);
+  //Commands to waiting and load navdata necessary variables
+  if (displacement == undefined) {
+    mission.altitude(1.5)
+    .hover(1000)
+    mission.run()
+
+    //recursive run
+    run();
+}
+  console.log("currentYaw", currentYaw);
+  console.log("displacement", displacement);
+  console.log("distance", distance);
+  console.log("yawAdjustment", yawAdjustment);
 
   if (displacement > 1) {
     // Craft mission
     console.log("Crafting mision");
+
+    //Turn clockwise in the direction to face the waypoint
+    mission.cw(yawAdjustment)
+    .hover(1000)
+    mission.run()
+
+    //find an way to calibrate 
+
     mission.zero()
-      .up(altitude)
+      .up(0.5)
       .hover(1000)
-      .cw(yawAdjustment)
-      .forward(distance);
+      .forward(displacement)
 
     mission.run(function (err, result) {
       if (err) {
-        console.trace("Oops, something bad happened: %s", err.message);
+        console.log("Oops, something bad happened: %s", err.message);
         mission.client().stop();
-        mission.client().land();
-      } else {
+        //mission.client().land();
+      } else if (result) {
         io.sockets.emit('waypointReached', { lat: targetLat, lon: targetLon });
         console.log('Reached ', targetLat, targetLon);
-        stop();
+        stop()
       }
     });
   }
@@ -134,6 +151,7 @@ let handleNavData = async function (data) {
   currentLat = data.gps.latitude;
   currentLon = data.gps.longitude;
   currentYaw = data.demo.rotation.yaw;
+  
   shouldRotateTowardsWaypoint = true;
 
   if (targetLat == null || targetLon == null || currentYaw == null || currentLat == null || currentLon == null) return;
@@ -149,20 +167,16 @@ let handleNavData = async function (data) {
 
   // Find X and Y components of displacement vector
   displacementVector = $V([Math.cos(droneBearing) * distance, Math.sin(droneBearing) * distance]);
-
-  // Get angle of drone
-  droneAbsoluteBearing = getDroneAbsoluteBearing();
-  if (droneAbsoluteBearing === null) {
-    droneAbsoluteBearing = droneBearing;
-  }
+  
   // Calculate angle needed to rotate so that the drone is facing the waypoint
-  var yawAdjustment = droneBearing - droneAbsoluteBearing;
+  yawAdjustment = droneBearing;
 
   // Convert yaw adjsutment to degrees and normalize
   yawAdjustment *= 180 / Math.PI;
   while (yawAdjustment > 180) { yawAdjustment -= 360; }
   while (yawAdjustment < -180) { yawAdjustment += 360; }
-  currentYaw = yawAdjustment;
+
+  yawAdjustment -= currentYaw;
 
   if (shouldRotateTowardsWaypoint) {
     displacement = Math.hypot(displacementVector.elements[0], displacementVector.elements[1]);
